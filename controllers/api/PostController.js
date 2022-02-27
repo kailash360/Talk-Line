fe4b5e3f4dd1779acf36c41eb28ba2c9bb3b76c2
kailash360 <kailashkejriwal21@gmail.com>
2022-02-27 18:34:07 +0530

    Added controoller to delete a post

--- controllers/api/PostController.js
+++ controllers/api/PostController.js
@@ -1,10 +1,12 @@
-const { Post } = require('../../models')
+const mongoose = require('mongoose')
+const { Post, User } = require('../../models')
 const { catchAsync } = require('../../utils')
 
 exports.createPost = catchAsync(async(req, res) => {
 
     if (!req.body.title || !req.body.description) return res.json({ success: false, message: 'Title and description are required' })
 
+    // create the post
     const post = await Post.create({
         title: req.body.title,
         description: req.body.description,
@@ -14,6 +16,28 @@ exports.createPost = catchAsync(async(req, res) => {
         comments: []
     })
 
+    // Add the post to the list of posts by the user
+    const user = await User.findOne({ _id: req.headers._id })
+    user.posts.push(post._id)
+    await user.save()
+
     return res.json({ success: true, data: { post } })
 
+})
+
+exports.deletePost = catchAsync(async(req, res) => {
+
+    if (!req.params.id) return res.json({ success: false, message: 'Post ID is required' })
+
+    const post = await Post.findOne({ _id: req.params.id })
+    if (!post) return res.json({ success: false, message: 'Post not found' })
+
+    await Post.deleteOne({ _id: req.params.id })
+
+    //Remove the deleted post from the list of posts by the users
+    const user = await User.findOne({ _id: req.headers._id })
+    user.posts = user.posts.filter(id => !mongoose.Types.ObjectId(id).equals(mongoose.Types.ObjectId(req.params.id)))
+    await user.save()
+
+    return res.json({ success: true, message: 'Post deleted successfully' })
 })
--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const AuthController = require('../../controllers/auth/AuthController')
 const UserController = require('../../controllers/api/UserController')
 const PostController = require('../../controllers/api/PostController')
-const { validateToken } = require('../../middlewares')
+const { validateToken, isOwner } = require('../../middlewares')
 
 router.post('/register', AuthController.register)
 router.post('/authenticate', AuthController.authenticate)
@@ -12,5 +12,6 @@ router.post('/unfollow/:id', validateToken, UserController.unfollowUser)
 router.get('/user', validateToken, UserController.getProfile)
 
 router.post('/posts', validateToken, PostController.createPost)
+router.delete('/posts/:id', validateToken, isOwner, PostController.deletePost)
 
 module.exports = router
