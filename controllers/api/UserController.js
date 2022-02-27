d5ac8ab7fe711a70b538bafb195a35aa6b3946c2
kailash360 <kailashkejriwal21@gmail.com>
2022-02-27 17:45:37 +0530

    Added controller to unfollow user

--- controllers/api/UserController.js
+++ controllers/api/UserController.js
@@ -6,7 +6,7 @@ exports.followUser = catchAsync(async(req, res) => {
 
     if (!mongoose.isValidObjectId(req.params.id)) return res.json({ success: false, message: 'Invalid user id' })
 
-    //Check if the user to follow himself
+    //Check if the user is trying to follow himself/herself
     if (req.params.id === req.headers._id) return res.json({ success: false, message: 'User cannot follow himself/herself' })
 
     // Fetch the user who has followed
@@ -24,6 +24,37 @@ exports.followUser = catchAsync(async(req, res) => {
     followedUser.followers.push(req.headers._id)
     await followedUser.save()
 
-    return res.json({ success: true, message: 'Followers updated successfully' })
+    return res.json({
+        success: true,
+        message: 'Successfully followed the user'
+    })
 
+})
+
+exports.unfollowUser = catchAsync(async(req, res) => {
+
+    if (!mongoose.isValidObjectId(req.params.id)) return res.json({ success: false, message: 'Invalid user id' })
+
+    //Check if the user is trying to follow himself/herself
+    if (req.params.id === req.headers._id) return res.json({ success: false, message: 'User cannot unfollow himself/herself' })
+
+    // Fetch the user who has unfollowed
+    const unfollowingUser = await User.findOne({ _id: req.headers._id })
+
+    //Check if unfollowed user exists in the list of followers
+    if (!unfollowingUser.following.includes(req.params.id)) return res.json({ success: false, message: 'You do not follow the user already' })
+
+    // remove the unfollowed user from the list of following
+    unfollowingUser.following = unfollowingUser.following.filter(id => !mongoose.Types.ObjectId(id).equals(mongoose.Types.ObjectId(req.params.id)))
+    await unfollowingUser.save()
+
+    // Remove the id of the unfollowing user from the list of the followers for the person who got unfollowed
+    const unfollowedUser = await User.findOne({ _id: req.params.id })
+    unfollowedUser.followers = unfollowedUser.followers.filter(id => !mongoose.Types.ObjectId(id).equals(mongoose.Types.ObjectId(req.headers._id)))
+    await unfollowedUser.save()
+
+    return res.json({
+        success: true,
+        message: 'Successfully unfollowed the user'
+    })
 })
--- routes/api/index.js
+++ routes/api/index.js
@@ -7,5 +7,6 @@ router.post('/register', AuthController.register)
 router.post('/authenticate', AuthController.authenticate)
 
 router.post('/follow/:id', validateToken, UserController.followUser)
+router.post('/unfollow/:id', validateToken, UserController.unfollowUser)
 
 module.exports = router
