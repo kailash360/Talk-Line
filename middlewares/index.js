a917c3d3040d63352ebdb59add374bc64a6eb0a0
kailash360 <kailashkejriwal21@gmail.com>
2022-02-27 17:16:46 +0530

    Added middleware to verify token

---
+++ middlewares/index.js
@@ -0,0 +1,3 @@
+const validateToken = require('./validateToken')
+
+module.exports = { validateToken }
---
+++ middlewares/validateToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models')
+const { catchAsync } = require('../utils')
+
+const validateToken = catchAsync(async(req, res, next) => {
+
+    jwt.verify(req.headers.token, process.env.JWT_KEY, async(err, decoded) => {
+
+        if (err) return res.json({ success: false, message: err.message });
+
+        const user = await User.findOne({ _id: decoded.user._id });
+        if (!user) return res.json({ success: false, message: 'User not found' })
+
+        req.headers = {...req.headers, ...decoded.user }
+        next()
+    })
+
+});
+
+module.exports = validateToken
