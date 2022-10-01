3fec0ad9c945a2696952a7a12f552b3a01a8d1da
Gurpreet Singh <lostlegend2701@gmail.com>
2022-10-01 11:24:17 +0530

    setup eslint

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": "off"
+    },
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+    }
+}
